f21796fd81fe180229bf3342d8b5ceba4b2fded7
erick <erick.becerra@uabc.edu.mx>
2021-02-22 18:25:01 -0800

    x
    x

---
+++ Practica 2/Assets/Ejercicio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Ejercicio:
+    bool Retorno(string[] Array, string Nom, int NumAlumnos){
+        for (int i = 0; i < NumAlumnos; i++)
+            if (Array[i] == Nom)
+                return true;
+        return false;
+    }
+}
