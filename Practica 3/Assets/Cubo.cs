b3cf8ade97f3ed516146fb5a735d8ecc7489bc2b
erick <erick.becerra@uabc.edu.mx>
2021-03-05 00:40:26 -0800

    x
    x

---
+++ Practica 3/Assets/Cubo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubo : Interactable
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+
+    public override void Interact()
+    {
+        Destroy (this.gameObject, 1);
+    }
+}
---
+++ Practica 3/Assets/Ejercicio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int[] SumaDos(int[] nums, int target) {
+        int[] resultado = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+            for (int j = 0; j < nums.Length; j++) {
+                if (i != j)
+                    if ((nums[i] + nums[j]) == target) {
+                        resultado[0] = i;
+                        resultado[1] = j;
+                    }
+            }
+        return resultado;
+    }
+}
