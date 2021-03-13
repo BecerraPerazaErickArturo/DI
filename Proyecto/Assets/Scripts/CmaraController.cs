4bbd0c9729e9a988f69adec3134568894a8bfc48
erick <erick.becerra@uabc.edu.mx>
2021-03-12 20:48:37 -0800

    x
    Practica 4

---
+++ Proyecto/Assets/Scripts/CmaraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CmaraController : MonoBehaviour
+{
+    public float mouseSensivity = 100f;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensivity, 0, 0);
+        player.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensivity, 0);
+    }
+}
